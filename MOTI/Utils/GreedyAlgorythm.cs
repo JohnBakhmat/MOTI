1b151188523014de26a43969bf016ae4bdd3b6dc
Yevhenii <yevhenii.bakhmat@nure.ua>
2021-12-19 10:57:29 +0200

    Fixed the greedyalgorythm

--- MOTI/Utils/GreedyAlgorythm.cs
+++ MOTI/Utils/GreedyAlgorythm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
+using Microsoft.AspNetCore.Components.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
 using MOTI.Data;
@@ -12,39 +13,56 @@ using NuGet.Protocol;
 
 namespace MOTI.Utils {
     public static class GreedyAlgorythm {
-        public static async Task<IEnumerable<Device>> OptimizeDevices(ApplicationDbContext context, int roomId, int requestId) {
+        public static async Task<IEnumerable<Device>> OptimizeDevices(ApplicationDbContext context,
+            int roomId,
+            int requestId) {
             //Fetch data
             var room = await context.Rooms.FirstOrDefaultAsync(r => r.RoomId == roomId);
-            var request =  await context.Requests.FirstOrDefaultAsync(r => r.RequestId == requestId);
-            var devices = await context.Devices.Where(d => d.RoomId == roomId).ToListAsync();
-            var climateSettings = await context.ClimateSettings.Where(cs => cs.RequestId == requestId).ToListAsync();
+            var request = await context.Requests.FirstOrDefaultAsync(r => r.RequestId == requestId);
+            var climateSettings = await context.ClimateSettings.Where(cs => cs.RequestId == requestId)
+                .ToListAsync();
+            var types = climateSettings.Select(cs => cs.ClimateType)
+                .ToList();
+            var devices = await context.Devices.Where(d => d.RoomId == roomId && types.Contains(d.ClimateType))
+                .ToListAsync();
+            devices.Sort((device,
+                device1) => Convert.ToInt32(Math.Round(device.Consumption - device1.Consumption)));
+            
             var result = new List<Device>();
             
-            devices.Sort((device, device1) => Convert.ToInt32(Math.Round(device.Capacity - device1.Capacity)));
-            double time = 0f;
-            foreach (var device in devices) {
-                result.Add(device);
-                time = GetTime(result, climateSettings[0].Value);
-                Console.WriteLine($"###: {result.Count} {time} {request.MaxTime}");
-                if (time <= request.MaxTime) {
+            var timingsList = new List<double>();
+            foreach (var climateSetting in climateSettings) {
+                var tempResult = new List<Device>();
+                foreach (var device in devices.Where(d=>d.ClimateType.Equals(climateSetting.ClimateType))) {
+                    tempResult.Add(device);
+                    var totalTime = GetTime(tempResult, climateSetting.Value);
+                    if (!(totalTime <= request.MaxTime)) continue;
+                    timingsList.Add(totalTime);
+                    result.AddRange(tempResult);
                     break;
                 }
             }
-
-            Console.WriteLine($"Device Count: {result.Count}; Time: {time}");
+            
+            Console.WriteLine("Devices: ");
+            foreach (var device in result) {
+                Console.Write($"{device.SerialNumber} ");
+            }
+            Console.WriteLine(";");
+            Console.WriteLine("Time spent: {0}", timingsList.Max());
+            Console.WriteLine("Total consumption:{0}", GetConsumption(result));
             return result;
         }
 
-        private static double GetTime(IEnumerable<Device> devices, double changes) {
-            var sumCapacity = 0.0;
-            foreach (var device in devices) {
-                if (device.Status == DeviceStatus.Working) {
-                    sumCapacity += device.Capacity;
-                }
-            }
-            Console.WriteLine($"*** Capacity: {sumCapacity}");
+        
+        private static double GetTime(IEnumerable<Device> devices,
+            double changes) {
+            var sumCapacity = devices.Where(device => device.Status == DeviceStatus.Working).Sum(device => device.Capacity);
+            // Console.WriteLine($"*** Capacity: {sumCapacity}");
             var sumTime = changes / sumCapacity;
             return sumTime;
         }
+
+        private static double GetConsumption(IEnumerable<Device> devices) =>
+            devices.Sum(device => device.Consumption);
     }
 }
